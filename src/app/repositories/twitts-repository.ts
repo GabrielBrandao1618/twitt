d2338ec5adbe61ed061053c68ff0cb3027b6a85f
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-30 13:29:58 -0300

    Configurações typescript adicionais realizadas

--- src/app/repositories/twitts-repository.ts
+++ src/app/repositories/twitts-repository.ts
@@ -1,5 +1,4 @@
 import { Twitt } from '../entities/twitt';
-
 export abstract class TwittsRepository {
   abstract create(twitt: Twitt): Promise<void>;
   abstract findById(twittId: string): Promise<Twitt | null>;
