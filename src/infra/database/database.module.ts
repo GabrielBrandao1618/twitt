6153ef71e90b966f8ad3fbcf235dd814baba240e
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-12-31 13:27:43 -0300

    (feat) Setup database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaUsersRepository } from './prisma/repositories/prisma-users-repository';
+import { PrismaTwittsRepository } from './prisma/repositories/prisma-twitts-repository';
+import { UsersRepository } from '@app/repositories/users-repository';
+import { TwittsRepository } from '@app/repositories/twitts-repository';
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UsersRepository,
+      useClass: PrismaUsersRepository,
+    },
+    {
+      provide: TwittsRepository,
+      useClass: PrismaTwittsRepository,
+    },
+  ],
+  exports: [UsersRepository, TwittsRepository],
 })
 export class DatabaseModule {}
